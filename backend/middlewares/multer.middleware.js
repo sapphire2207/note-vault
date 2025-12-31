2a9d5adde01b9ab7bb5de9b25fbcffa3cfb77cf0
Mysore Sridhar <118914178+sapphire2207@users.noreply.github.com>
2025-12-31 21:49:03 +0530

    Refactor multer middleware for improved path handling

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -1,14 +1,13 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp")
+        cb(null, path.join(process.cwd(), "public", "temp"));
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + file.originalname)
+        cb(null, Date.now() + "-" + file.originalname);
     }
-})
-  
-export const upload = multer({ 
-    storage,
 });
+
+export const upload = multer({ storage });
